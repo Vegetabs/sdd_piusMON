f42d6b58d1589be6ac9dfb5f826b57fc326180ca
murp31 <murp31@stpiusx.nsw.edu.au>
2024-06-05 23:01:41 +1000

    added enemy mon scene

---
+++ scripts/e_mon_char.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+@export var team_name := ""
+@onready var mon_data = []
+@onready var mon_sprite = $mon_sprite
+@onready var anim = $anim_player
+@onready var posx = self.position.x
+@onready var posy = self.position.y
+var mon_cache := []
+var mon_state = 0 #--State = 0 (alive) or 1 (dead)--#
+
+func _ready():	
+	pass
+
+func setup_mon(mon_info:Array) -> void:
+	mon_data = mon_info
+	change_mon_texture(mon_data[0])
+
+func change_mon_texture(mon_id:int) -> void:
+	mon_sprite.texture = load(ResourcePaths.get_mon_path(mon_id))
+	mon_sprite.offset = Vector2(mon_sprite.texture.get_width()*-1,mon_sprite.texture.get_height()*-1)
+
+func attack() -> void:
+	anim.play("attack")
+	var tween = get_tree().create_tween()
+	tween.set_ease(2)
+	tween.tween_property(self,"position",Vector2(posx-200,posy),1)
+	tween.tween_property(self,"position",Vector2(posx,posy),0.5)
+
+func swap(new_mon:Array) -> void:
+	mon_cache = new_mon
+	anim.play("swap")
+
+func hit(dmg:int) -> void:
+	if _set_health(dmg):
+		mon_state = 1
+		anim.play("death")
+	else:
+		anim.play("hit")
+
+func _set_health(val:int) -> bool:
+	if mon_data[2]-val <= 0:
+		mon_data[2] = 0
+		return true
+	else:
+		mon_data[2] -= val
+		return false
+
+func _on_anim_player_animation_finished(anim_name):
+	if anim_name == "attack":
+		pass
+	elif anim_name == "swap":
+		setup_mon(mon_cache)
+		mon_cache = []
+	elif anim_name == "hit":
+		pass
+	elif anim_name == "death":
+		SignalBus.mon_death.emit(team_name)
